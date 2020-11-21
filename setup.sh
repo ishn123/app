f79da115cbce8cbaa014c68a2c30669d788d2a6a
ishn123 <62070580+ishn123@users.noreply.github.com>
2020-11-21 13:49:46 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"myemail@example.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
