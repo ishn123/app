8ab722eb32c187edde1277312c3e0897eb59b15e
ishn123 <62070580+ishn123@users.noreply.github.com>
2020-11-22 18:56:44 +0530

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -1,8 +1,10 @@
 import psycopg2 as pg
 
-Database_url = 'postgres://yctlzmuuxmgyeo:8e1474edfd4c01994ff7f8b496b9fc3619f1c3e1f312b36e2295b0eb9a8aa85a@ec2-18-203-62-227.eu-west-1.compute.amazonaws.com:5432/d3v85adstclkte'
 
-db = pg.connect(Database_url,sslmode='require')
+db = pg.connect(host='ec2-18-203-62-227.eu-west-1.compute.amazonaws.com',
+                database='d3v85adstclkte',
+                user='yctlzmuuxmgyeo',
+                password='8e1474edfd4c01994ff7f8b496b9fc3619f1c3e1f312b36e2295b0eb9a8aa85a')
 
 connection = db.cursor()
 if connection:
@@ -18,32 +20,40 @@ def create_user():
         Username varchar NOT NULL UNIQUE,
         password varchar NOT NULL
     )''')
-
+    db.commit()
+    db.close()
 def add_user_data(first_name,last_name,emailaddress,user,password):
     connection.execute('''INSERT INTO Data(FirstName,LastName,emailaddress,Username,password) values (?,?,?,?,?)''',(first_name,last_name,emailaddress,user,password))
     db.commit()
+    db.close()
 def display_data():
     connection.execute('''SELECT * FROM Data''')
     rows = connection.fetchall()
+    db.close()
     return rows
 def delete_data():
     connection.execute('''delete from Data''')
     print('Deleted Successfully')
     db.commit()
+    db.close()
 def column_data(column_name = None):
     if column_name == 'emailaddress':
         connection.execute('''select emailaddress from Data''')
         col = connection.fetchall()
+        db.close()
         return col
     elif column_name == 'username':
         connection.execute('''select Username from Data''')
         col = connection.fetchall()
+        db.close()
         return col
     else:
         connection.execute('''select * from Data''')
         rows = connection.fetchall()
+        db.close()
         return rows
 def two_columns_retrieval(user,passe):
     connection.execute('''select * from Data where Username=? and password=?''',(user,passe))
     rows =  connection.fetchall()
+    db.close()
     return rows
