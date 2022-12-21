3c7df0efe12746cbf50a85b82765ed7ae3b0b24e
ishn123 <aroraishan51@gmail.com>
2022-12-21 19:36:03 +0530

    Updated DB connection

--- Untitled-1.py
+++ Untitled-1.py
@@ -7,13 +7,15 @@ import matplotlib.cm as cm
 import hashlib
 from database_connection import *
 
+
 def hashing(password):
     return hashlib.sha256(str.encode(password)).hexdigest()
+
 
 def make_gradcam_heatmap(img_array, model, last_conv_layer_name, classifier_layer_names):
     # First, we create a model that maps the input image to the activations
     # of the last conv layer
-    img = np.expand_dims(img_array,0)
+    img = np.expand_dims(img_array, 0)
     last_conv_layer = model.get_layer(last_conv_layer_name)
     last_conv_layer_model = keras.Model(model.inputs, last_conv_layer.output)
 
@@ -42,7 +44,7 @@ def make_gradcam_heatmap(img_array, model, last_conv_layer_name, classifier_laye
 
     # This is a vector where each entry is the mean intensity of the gradient
     # over a specific feature map channel
-    pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
     last_conv_layer_output = last_conv_layer_output[0].numpy()
     pooled_grads = pooled_grads.numpy()
     for i in range(pooled_grads.shape[-1]):
@@ -51,10 +53,11 @@ def make_gradcam_heatmap(img_array, model, last_conv_layer_name, classifier_laye
     # The channel-wise mean of the resulting feature map
     # is our heatmap of class activation
     heatmap = np.mean(last_conv_layer_output, axis=-1)
-    #heatmap = cv2.resize(heatmap,(224,224),cv2.INTER_LINEAR)
+    # heatmap = cv2.resize(heatmap,(224,224),cv2.INTER_LINEAR)
     # For visualization purpose, we will also normalize the heatmap between 0 & 1
     heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
     return heatmap
+
 
 def superimposed_img(image, heatmap):
     # We rescale heatmap to a range 0-255
@@ -69,7 +72,7 @@ def superimposed_img(image, heatmap):
 
     # We create an image with RGB colorized heatmap
     jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
-    jet_heatmap = jet_heatmap.resize((224,224))
+    jet_heatmap = jet_heatmap.resize((224, 224))
     jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
 
     # Superimpose the heatmap on original image
@@ -77,7 +80,8 @@ def superimposed_img(image, heatmap):
     superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
     return superimposed_img
 
-def frontend(gui = True):
+
+def frontend(gui=True):
     if gui:
         im1 = Image.open('random.PNG')
         st.title('Swasthya setu')
@@ -92,42 +96,43 @@ def frontend(gui = True):
                 chapter, we discussed state of the art deep learning architecture and its
                 optimization used for medical image segmentation and classification.
                 © made by ISHAN ARORA CSE Department FET,GKV Haridwar''')
-        st.image([im1],width=600)
+        st.image([im1], width=600)
         st.write('\n')
         st.subheader('Steps to upload your X-ray')
         st.markdown('1. Go to the side bar and Login/SignUp.\n 2. After Login in Upload your X-ray.')
-        option = st.sidebar.selectbox(label='Menu',options=['Create account','Login'],)
-        if option =='Create account':
+        option = st.sidebar.selectbox(label='Menu', options=['Create account', 'Login'], )
+        if option == 'Create account':
             create_user()
             first_name = st.sidebar.text_input(label='First Name')
             last_name = st.sidebar.text_input(label='Last Name')
             Emailaddress = st.sidebar.text_input(label='Email Address')
             if Emailaddress != '':
-                uniqueess_checker(Emailaddress,'emailaddress')
+                uniqueess_checker(Emailaddress, 'emailaddress')
             Username = st.sidebar.text_input(label='Username')
             if Username != '':
-                uniqueess_checker(Username,'username')
-            Password = st.sidebar.text_input(label='Password',type='password')
+                uniqueess_checker(Username, 'username')
+            Password = st.sidebar.text_input(label='Password', type='password')
             Signup = st.sidebar.button('Signup')
             if Signup:
                 encrypt = hashing(Password)
-                add_user_data(first_name,last_name,Emailaddress,Username,encrypt)
+                add_user_data(first_name, last_name, Emailaddress, Username, encrypt)
                 st.success('Account created successfully.Now go to login to upload your X-ray.')
         else:
-            username = st.sidebar.text_input(label='Username',key='Username')
-            password = st.sidebar.text_input(label='password',key='password',type='password')
+            username = st.sidebar.text_input(label='Username', key='Username')
+            password = st.sidebar.text_input(label='password', key='password', type='password')
             login = st.sidebar.checkbox(label='Login')
             if login:
                 hshd_pss = hashing(password)
-                rows = two_columns_retrieval(username,hshd_pss)
-                if rows !=[]:
+                rows = two_columns_retrieval(username, hshd_pss)
+                if rows != []:
                     st.success('Login successfully as {}'.format(username))
                     st.write('Now you can upload your X-ray.')
                     prediction()
                 else:
                     st.warning('Username/Password is Incorrect')
-        
-def uniqueess_checker(column_argument,column):
+
+
+def uniqueess_checker(column_argument, column):
     col = column_data(column)
     for a in col:
         if column_argument == a[0]:
@@ -138,28 +143,32 @@ def uniqueess_checker(column_argument,column):
                 st.sidebar.warning('Username already exists,Try with diiferent one')
                 break
 
-def prediction():
 
-    upload = st.file_uploader(label='',type = ['png','jpg','jpeg'])
+def prediction():
+    upload = st.file_uploader(label='', type=['png', 'jpg', 'jpeg'])
     analyze = st.button('Analyze report')
     if analyze:
         cnn_model = keras.models.load_model('my_model.h5')
         with st.spinner('Wait for a while'):
-            img = Image.open(upload,'r')
-            img = img.resize((224,224))
+            img = Image.open(upload, 'r')
+            img = img.resize((224, 224))
             img = np.array(img)
-            img = img.reshape(224,224,3)
-            img = img/255
-            pred = cnn_model.predict(np.expand_dims(img,0))
+            img = img.reshape(224, 224, 3)
+            img = img / 255
+            pred = cnn_model.predict(np.expand_dims(img, 0))
             if np.argmax(pred) == 1:
                 st.write('You have been diagonsed with **_COVID-19 !_** Please consult Doctor immediately.')
-                heatmap=make_gradcam_heatmap(img,cnn_model,last_conv_layer_name='conv_pw_13_relu',classifier_layer_names=['global_average_pooling2d','dense_1'])
+                heatmap = make_gradcam_heatmap(img, cnn_model, last_conv_layer_name='conv_pw_13_relu',
+                                               classifier_layer_names=['global_average_pooling2d', 'dense_1'])
                 ima = np.uint8(255 * img)
-                s_img = superimposed_img(ima,heatmap)
+                s_img = superimposed_img(ima, heatmap)
                 s_img.save('img1.png')
-                display_img = Image.open('img1.png','r')
-                st.image(display_img,width = 150,caption = st.write('**_Red_** marks in X-ray indicates swelling in Bronchitis.'))
+                display_img = Image.open('img1.png', 'r')
+                st.image(display_img, width=150,
+                         caption=st.write('**_Red_** marks in X-ray indicates swelling in Bronchitis.'))
             else:
                 st.write('Your X-ray is **_Normal_**.')
+
+
 if __name__ == '__main__':
     frontend()
--- database_connection.py
+++ database_connection.py
@@ -1,16 +1,13 @@
-import psycopg2 as pg
+import sqlite3
+db = sqlite3.connect("db")
 
-
-db = pg.connect(host='ec2-34-198-223-125.compute-1.amazonaws.com',
-                database='dcj6rc24m2uosi',
-                user='gqcpefohdiotvm',
-                password='d75c41226a88b6bb8e68a6c30fde85b6592a1c5fbaf5e5b9ba6860136911ec24')
-
-connection = db.cursor()
-if connection:
+connection = None
+try:
+    connection = db.cursor()
     print('Connection established')
-else:
+except Exception as e:
     print('Connection Failed')
+
 
 def create_user():
     connection.execute('''CREATE TABLE IF NOT EXISTS Data(
@@ -21,22 +18,29 @@ def create_user():
         password varchar NOT NULL
     )''')
     db.commit()
-    
-def add_user_data(first_name,last_name,emailaddress,user,password):
-    connection.execute('''INSERT INTO Data(FirstName,LastName,emailaddress,Username,password) values (%s,%s,%s,%s,%s)''',(first_name,last_name,emailaddress,user,password))
+
+
+def add_user_data(first_name, last_name, emailaddress, user, password):
+    connection.execute(
+        '''INSERT INTO Data(FirstName,LastName,emailaddress,Username,password) values (%s,%s,%s,%s,%s)''',
+        (first_name, last_name, emailaddress, user, password))
     db.commit()
+
 
 def display_data():
     connection.execute('''SELECT * FROM Data''')
     rows = connection.fetchall()
-    
+
     return rows
+
+
 def delete_data():
     connection.execute('''delete from Data''')
     print('Deleted Successfully')
     db.commit()
-    
-def column_data(column_name = None):
+
+
+def column_data(column_name=None):
     if column_name == 'emailaddress':
         connection.execute('''select emailaddress from Data''')
         col = connection.fetchall()
@@ -44,15 +48,17 @@ def column_data(column_name = None):
     elif column_name == 'username':
         connection.execute('''select Username from Data''')
         col = connection.fetchall()
-        
+
         return col
     else:
         connection.execute('''select * from Data''')
         rows = connection.fetchall()
-        
+
         return rows
-def two_columns_retrieval(user,passe):
-    connection.execute('''select * from Data where Username=%s and password=%s''',(user,passe))
-    rows =  connection.fetchall()
-    
+
+
+def two_columns_retrieval(user, passe):
+    connection.execute('''select * from Data where Username=%s and password=%s''', (user, passe))
+    rows = connection.fetchall()
+
     return rows
